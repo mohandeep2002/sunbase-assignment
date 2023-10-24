874fc5ca4f63ad5f607faabcea811c4898865ed3
Mohandeep Atmuri <mohandeep2002@gmail.com>
2023-10-24 17:45:14 +0530

    Working on DOTween Package

--- Assets/Scripts/Controllers/DisplayController.cs
+++ Assets/Scripts/Controllers/DisplayController.cs
@@ -2,17 +2,25 @@ using UnityEngine;
 using Sunbase.Class;
 using System.Collections.Generic;
 using TMPro;
+using DG.Tweening;
+using UnityEngine.UI;
 
 namespace Sunbase.Controllers
 {
     public class DisplayController : MonoBehaviour
     {
-        [Header("Data Panel")]
+        [Header("Data Panel Variables")]
         public GameObject dataPrefab;
         public Transform content;
         public List<GameObject> generatedUI = new List<GameObject>();
         public Transform errorPopup;
+        [Header("Dropdown Variables")]
+        public Transform panelToPopUp;
+        public float animationDuration = 0.5f;
+        public float fadeDuration = 1f;
+        public bool isPanelVisible = false;
 
+        #region Main Data Display
         public void ShowData(SunbaseClass tempData)
         {
             foreach (Client client in tempData.clients)
@@ -48,6 +56,27 @@ namespace Sunbase.Controllers
             textInPopup.text = message;
             errorPopup.gameObject.SetActive(true);
         }
+        #endregion
+
+        private void Start()
+        {
+            TogglePopUp();
+        }
+
+        #region DropDown Display
+        public void TogglePopUp()
+        {
+            if (isPanelVisible)
+            {
+                panelToPopUp.DOMoveY(panelToPopUp.position.y - 500f, animationDuration);
+            }  
+            else
+            {
+                panelToPopUp.DOMoveY(panelToPopUp.position.y + 500f, animationDuration);
+            }
+            isPanelVisible = !isPanelVisible;
+        }
+        #endregion
     }
 }
 
